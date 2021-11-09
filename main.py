76a86c85dec2ee0c0a88d399d3d9c0590be23b71
STUDENT <cse.saideepika9@gmail.com>
2021-11-09 15:51:30 +0530

    madd

--- main.py
+++
@@ -1,9 +0,0 @@
-def check_palindrome(str1):
-	if str1[::-1]==str1:
-		return True
-	return False
-str1=input()
-if check_palindrome(str1)==True
-	print("palindrome")
-else:
-	print("not palindrome")
--- main1.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter number))
-p==n**(1/2)
-for i in range(2):
-	m=n/p
-if(m==p)
-	print("perfect square")
-else;
-	print("not perfect square")
